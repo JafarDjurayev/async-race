3e5c69576542cbd0b8c04c5a16b2fc556404397a
JafarDjurayev <djorayevofficial@gmail.com>
2025-06-26 23:31:44 +0500

    refactor: Changed layout of Garage component's structure and reduced code line from 50+ to 20. Created new RaceLane component and moved some logical structure from GaragePage.

---
+++ src/components/RaceLane.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../styles/RaceLane.css';
+import carIcon from '../assets/racing-car-with-side-skirts-svgrepo-com.svg';
+
+export default function RaceLane(){
+    return(<>
+    <div className="race-lane">
+            <div className="car-control-panel">
+              <div>
+                <button type="button" className="car-select-button">
+                  SELECT
+                </button>
+                <br />
+                <br />
+                <button type="button" className="car-remove-button">
+                  REMOVE
+                </button>
+              </div>
+              <div>
+                <button type="button" className="car-control-button">
+                  S
+                </button>
+                <br />
+                <br />
+                <button type="button" className="car-control-button">
+                  R
+                </button>
+              </div>
+            </div>
+            <div className="track">
+              <div>
+                <img src={carIcon} alt="car" style={{ width: '100px', height: '90px' }}/>
+              </div>
+              <h3>BMW i5</h3>
+            </div>
+          </div>
+    </>)
+}
--- src/pages/garage/GaragePage.tsx
+++ src/pages/garage/GaragePage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Controls from '../../components/Controls';
 import Navigation from '../../components/Navigation';
-import carIcon from '../../assets/racing-car-with-side-skirts-svgrepo-com.svg';
+import RaceLane from '../../components/RaceLane';
 import '../../styles/Garage.css';
 
 export default function GaragePage() {
@@ -12,40 +12,7 @@ export default function GaragePage() {
         <Controls />
         <div className="race-lanes">
           <h1 className="race-title">START</h1>
-          <div className="race-lane">
-            <div className="car-control-panel">
-              <div>
-                <button type="button" className="car-select-button">
-                  SELECT
-                </button>
-                <br />
-                <br />
-                <button type="button" className="car-remove-button">
-                  REMOVE
-                </button>
-              </div>
-              <div>
-                <button type="button" className="car-control-button">
-                  S
-                </button>
-                <br />
-                <br />
-                <button type="button" className="car-control-button">
-                  R
-                </button>
-              </div>
-            </div>
-            <div className="track">
-              <div>
-                <img
-                  src={carIcon}
-                  alt="car"
-                  style={{ width: '100px', height: '90px' }}
-                />
-              </div>
-              <h3>BMW i5</h3>
-            </div>
-          </div>
+          <RaceLane />
         </div>
       </div>
     </>

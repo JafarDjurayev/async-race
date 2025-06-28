6ccf664caf671293189b0f712737f41760f1b662
JafarDjurayev <djorayevofficial@gmail.com>
2025-06-28 13:41:25 +0500

    feat:implement pagination with 7 cars per page and navigation buttons

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { type JSX } from 'react';
 import '../styles/Pagination.css';
+import {type PaginationProps } from '../types/models';
 
-export default function Pagination() {
+export default function Pagination({currentPage,totalPages,onPageChange,}: PaginationProps): JSX.Element {
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
   return (
-    <>
-      <div className="pagination">
-        <button disabled className="button-control">
-          &lt; Prev
+    <div className="pagination">
+      <button
+        className="button-control"
+        onClick={() => onPageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        &lt; 
+      </button>
+      {pages.map((page) => (
+        <button
+          key={page}
+          className={page === currentPage ? 'active' : ''}
+          onClick={() => onPageChange(page)}
+        >
+          {page}
         </button>
-        <button className="active">1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button className="button-control">Next &gt;</button>
-      </div>
-    </>
+      ))}
+
+      <button
+        className="button-control"
+        onClick={() => onPageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}
+      >
+         &gt;
+      </button>
+    </div>
   );
 }
+

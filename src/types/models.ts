98a002ee92329f62d9cea7897f8607a9024a1685
JafarDjurayev <djorayevofficial@gmail.com>
2025-06-28 15:03:44 +0500

    feat: Implemented Interfaces, types for: PaginationStore, 100 random cars, CarsStore and props for Pagination

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,13 +1,44 @@
+import { fetchCars } from "../api/garage";
+
 export interface Car {
+  id: number;
   name: string;
   color: string;
-  id: number;
 }
 
 export interface Winners {
   id: number;
   wins: number;
   time: number;
+}
+
+export type GenerateCarsHook = {
+  handleGenerateClick: () => Promise<void>;
+};
+
+
+export interface PaginationStore {
+  currentPage: number;
+  totalPages: number;
+  setCurrentPage: (page: number) => void;
+  setTotalPages: (total: number) => void;
+}
+
+
+export interface CarStore {
+  cars: Car[];
+  setCars: (cars: Car[]) => void;
+  addCars: (newCars: Car[]) => void;
+}
+
+
+export const brands: string[] = ['Tesla', 'Ford', 'Toyota', 'BMW', 'Audi', 'Chevy', 'Honda', 'Nissan', 'Mazda', 'Mercedes']
+export const models: string[] = ['Model S', 'Mustang', 'Corolla', '3 Series', 'A4', 'Camaro', 'Civic', 'Altima', 'CX-5', 'C-Class'];
+
+export interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
 }
 
 export type EngineStatus = 'started' | 'stopped' | 'drive';

ba98cb80925ef2ab7bd611602debf16a9d4ee310
JafarDjurayev <djorayevofficial@gmail.com>
2025-06-30 21:00:48 +0500

    refactor: give to child props by render keys and cardId

--- src/components/RaceLane.tsx
+++ src/components/RaceLane.tsx
@@ -5,23 +5,31 @@ import CarControlPanel from './CarControlPanel';
 import { useCarStore } from '../app/zustand/useGarageStore';
 import { usePaginationStore } from '../app/zustand/useGarageStore';
 
+
 const CARS_PER_PAGE :number= 7;
+
+// Add this useEffect to track store changes
 
 export default function RaceLane() {
   const cars = useCarStore((state) => state.cars);
+ ;
+
+  console.log('Rendering cars:', cars);
+
   const currentPage = usePaginationStore((state) => state.currentPage);
   const setTotalPages = usePaginationStore((state) => state.setTotalPages);
   useEffect(() => {
     const pages = Math.ceil(cars.length / CARS_PER_PAGE);
     setTotalPages(pages);
   }, [cars, setTotalPages]);
+ 
   const startIndex = (currentPage - 1) * CARS_PER_PAGE;
   const currentCars = cars.slice(startIndex, startIndex + CARS_PER_PAGE);
   return (
     <>
       {currentCars.map((car) => (
         <div key={car.id} className="race-lane">
-         <CarControlPanel />
+          <CarControlPanel key={car.id} carId={car.id}/>
           <div className="track">
             <div>
               <CarIcon style={{ width: '100px', height: '90px', fill: car.color }} aria-label="car" />
@@ -33,3 +41,4 @@ export default function RaceLane() {
     </>
   );
 }
+

d3838acfd93dc8eb103fd7a3e0b56277995e15f2
JafarDjurayev <djorayevofficial@gmail.com>
2025-06-30 21:27:39 +0500

    feat: Implemented Select button's functionality to select car for updates. Implemented click event for REMOVE button to delete car

--- src/components/CarControlPanel.tsx
+++ src/components/CarControlPanel.tsx
@@ -1,25 +1,44 @@
-import React, {type JSX} from "react";
+import React, { type JSX } from "react";
 import '../styles/CarControlPanel.css';
+import { useCarStore, useCarUpdateStore } from "../app/zustand/useGarageStore";
+import { type CarControlPanelProps } from "../types/models";
+import { handleRemoveCar } from "../utils/GarageUtils";
 
-export default function CarControlPanel(): JSX.Element{
-    return(<>
-    <div className="car-control-panel">
+
+export default function CarControlPanel({ carId }: CarControlPanelProps): JSX.Element {
+
+    const cars = useCarStore((state) => state.cars);
+    const setSelectedCar = useCarUpdateStore((state) => state.setSelectedCar);
+  const handleSelectClick = () => {
+    const selected = cars.find((car) => car.id === carId);
+    if (selected) {
+      setSelectedCar(selected);
+      console.log('Selected car:', selected);
+    }else {
+    console.warn('No car found with ID:', carId); // ⚠️ Debug: invalid ID?
+  }
+  };
+    const handleClick = () => {
+        void handleRemoveCar(carId);
+    };
+    return (<>
+        <div className="car-control-panel">
             <div>
-              <button type="button" className="car-select-button">
-                SELECT
-              </button>
-              <br />
-              <br />
-              <button type="button" className="car-remove-button">
-                REMOVE
-              </button>
+                <button type="button" className="car-select-button" onClick={handleSelectClick}>
+                    SELECT
+                </button>
+                <br />
+                <br />
+                <button type="button" className="car-remove-button" onClick={handleClick}>
+                    REMOVE
+                </button>
             </div>
             <div>
-              <button type="button" className="car-control-button">S</button>
-              <br />
-              <br />
-              <button type="button" className="car-control-button">R</button>
+                <button type="button" className="car-control-button">S</button>
+                <br />
+                <br />
+                <button type="button" className="car-control-button">R</button>
             </div>
-          </div>
+        </div>
     </>)
 }

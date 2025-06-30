a784b8c21a512bf6a865089f2e0b140d4d09fa52
JafarDjurayev <djorayevofficial@gmail.com>
2025-06-30 21:41:19 +0500

    feat: Created types and interfaces for Car creation, update. Created types also for Engine and Race handling functions

--- src/types/models.ts
+++ src/types/models.ts
@@ -5,6 +5,11 @@ export interface Car {
   color: string;
 }
 
+export interface CarCreate {
+  name: string;
+  color: string;
+}
+
 export interface Winners {
   id: number;
   wins: number;
@@ -46,10 +51,30 @@ export interface WinnersPaginationStore {
 
 export interface CarStore {
   cars: Car[];
+  carNameInput: string;
+  carColorInput: string;
+
   setCars: (cars: Car[]) => void;
   addCars: (newCars: Car[]) => void;
+  removeCar: (id: number) => void;
+  updateCar: (car: Car) => void;
+
+  setCarNameInput: (name: string) => void;
+  setCarColorInput: (color: string) => void;
 }
 
+
+
+export interface CarCreateStore {
+  cars: Car[];
+  carNameInput: string;
+  carColorInput: string;
+  setCarNameInput: (name: string) => void;
+  setCarColorInput: (color: string) => void;
+  addCars: (newCars: Car[]) => void;
+  addCar: (car: Car) => void;
+  createCar: () => Promise<void>;
+}
 
 export const brands: string[] = ['Tesla', 'Ford', 'Toyota', 'BMW', 'Audi', 'Chevy', 'Honda', 'Nissan', 'Mazda', 'Mercedes']
 export const models: string[] = ['Model S', 'Mustang', 'Corolla', '3 Series', 'A4', 'Camaro', 'Civic', 'Altima', 'CX-5', 'C-Class'];
@@ -61,3 +86,25 @@ export interface PaginationProps {
 }
 
 export type EngineStatus = 'started' | 'stopped' | 'drive';
+
+export interface EngineStartResponse {
+  velocity: number;
+  distance: number;
+}
+export type EngineError = 'broken' | 'in-progress' | 'not-found';
+
+export interface EngineDriveResponse {
+  success: boolean;
+}
+
+export interface CarControlPanelProps {
+  carId: number;
+}
+
+export interface CarUpdateStore {
+  selectedCar: Car | null;
+  setSelectedCar: (car: Car) => void;
+  clearSelectedCar: () => void;
+}
+
+

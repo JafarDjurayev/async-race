73b16b6f916e048b64d2a3aa2cf0854620e77d48
JafarDjurayev <djorayevofficial@gmail.com>
2025-06-30 21:28:45 +0500

    feat: Implemented functionality for buttons to Creat, Update cars

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,9 +1,17 @@
 import React, {type JSX} from 'react';
 import '../styles/Controls.css';
 import { useGenerateCars } from './HandleGenerateCars';
+import { CreateCarStore} from '../app/zustand/useGarageStore';
+import { useCarUpdateHandler } from '../utils/GarageUtils';
+import { useSyncCarInputs } from './HandleCarChanges';
 
 export default function Controls(): JSX.Element {
-   const { handleGenerateClick } = useGenerateCars();
+  const { nameValue, colorValue, handleNameChange,handleColorChange } = useSyncCarInputs();
+  const { handleGenerateClick } = useGenerateCars();
+  const setCarName = CreateCarStore((state) => state.setCarNameInput);
+  const setCarColor = CreateCarStore((state) => state.setCarColorInput);
+  const createCar = CreateCarStore((state) => state.createCar);
+  const { handleUpdateClick } = useCarUpdateHandler();
   return (
     <>
       <div className="controls">
@@ -11,19 +19,15 @@ export default function Controls(): JSX.Element {
           <button type="button">RACE</button>
           <button type="button">RESET</button>
         </div>
-        <form>
-          <input type="text" placeholder="Car name" className="input-name" />
-          <input type="color" />
-          <button type="button" className="button-create">
-            CREATE
-          </button>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input type="text" placeholder="Car name" className="input-name" onChange={(e) => setCarName(e.target.value)}/>
+          <input type="color"  onChange={(e) => setCarColor(e.target.value)}/>
+          <button type="button" className="button-create" onClick={createCar}>CREATE</button>
         </form>
-        <form>
-          <input type="text" placeholder="Car name" className="input-name" />
-          <input type="color" />'
-          <button type="button" className="button-update">
-            UPDATE
-          </button>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input type="text" placeholder="Car name" className="input-name" value={nameValue}  onChange={handleNameChange}/>
+          <input type="color" className="color-input" value={colorValue}  onChange={handleColorChange}/>
+          <button type="button" className="button-update" onClick={handleUpdateClick}>UPDATE</button>
         </form>
         <button type="button" onClick={handleGenerateClick}>GENERATE</button>
       </div>

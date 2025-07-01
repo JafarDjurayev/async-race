3cf52e96df5346ffe914e96960e9c453b69dbd0e
JafarDjurayev <djorayevofficial@gmail.com>
2025-07-01 22:14:44 +0500

    fix: Deleted Navigation from router as it was duplicating layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,8 @@
-// // import type { JSX } from 'react/jsx-runtime';
-// import { Routes, Route } from 'react-router-dom'
-// import Layout from './layout/Layout';
-// import GaragePage from './pages/garage/GaragePage';
-// import WinnersPage from './pages/winners/WinnersPage';
-// // import AppRouter from './routes/AppRouter';
-
-// // function App(): JSX.Element {
-// //   return <AppRouter />;
-// // }
-
-// export function App() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Layout />}>
-//         <Route index element={<GaragePage />} />
-//         <Route path="winners" element={<WinnersPage />} />
-//       </Route>
-//     </Routes>
-//   )
-// }
-
-import Navigation from './components/Navigation'
 import AppRouter from './routes/AppRouter';
 
 function App() {
   return (
     <>
-      <Navigation />
       <AppRouter />
     </>
   )

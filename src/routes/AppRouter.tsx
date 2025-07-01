85917766edb572d684bb3adac9cd4050a4e456f4
JafarDjurayev <djorayevofficial@gmail.com>
2025-07-01 21:25:17 +0500

    refactor: removed unused router

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,18 +1,16 @@
 import { type JSX } from 'react';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import GaragePage from '../pages/garage/GaragePage';
 import WinnersPage from '../pages/winners/WinnersPage';
 import Layout from '../layout/Layout';
 
 export default function AppRouter(): JSX.Element {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<GaragePage />} />
-          <Route path="winners" element={<WinnersPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<GaragePage />} />
+        <Route path="winners" element={<WinnersPage />} />
+      </Route>
+    </Routes>
   );
 }

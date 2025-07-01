2109c7a8e52ba2525980dc3dd38ce7eb209ba5a8
JafarDjurayev <djorayevofficial@gmail.com>
2025-07-01 21:22:21 +0500

    fix: removed wrapperRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react';
-import { RouterWrapper } from './routes/router.tsx';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterWrapper>
       <App />
-    </RouterWrapper>
   </StrictMode>
 );

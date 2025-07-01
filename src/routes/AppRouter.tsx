545548dae77e577b290170edd20ec603ce287e91
JafarDjurayev <djorayevofficial@gmail.com>
2025-07-01 21:53:45 +0500

    refactor: changed structure of Routing to follow single routes

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,16 +1,16 @@
-// import { type JSX } from 'react';
-// import { Route, Routes } from 'react-router-dom';
-// import GaragePage from '../pages/garage/GaragePage';
-// import WinnersPage from '../pages/winners/WinnersPage';
-// import Layout from '../layout/Layout';
+import { type JSX } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import GaragePage from '../pages/garage/GaragePage';
+import WinnersPage from '../pages/winners/WinnersPage';
+import Layout from '../layout/Layout';
 
-// export default function AppRouter(): JSX.Element {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Layout />}>
-//         <Route index element={<GaragePage />} />
-//         <Route path="winners" element={<WinnersPage />} />
-//       </Route>
-//     </Routes>
-//   );
-// }
+export default function AppRouter(): JSX.Element {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<GaragePage />} />
+        <Route path="winners" element={<WinnersPage />} />
+      </Route>
+    </Routes>
+  );
+}

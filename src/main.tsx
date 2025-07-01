d994ac6ea5e5a5a5d6b7720d69e6195b2c62b482
JafarDjurayev <djorayevofficial@gmail.com>
2025-07-01 21:15:38 +0500

    feat: created configure single route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
+import { RouterWrapper } from './routes/router.tsx';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <RouterWrapper>
       <App />
-  </StrictMode>,
+    </RouterWrapper>
+  </StrictMode>
 );

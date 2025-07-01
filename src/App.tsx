e04b3970fd2469aa88e26a57d2528bd11b4ec8e2
JafarDjurayev <djorayevofficial@gmail.com>
2025-07-01 21:47:26 +0500

    refactor: trying to implement react router directly in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import type { JSX } from 'react/jsx-runtime';
-import AppRouter from './routes/AppRouter';
+// import type { JSX } from 'react/jsx-runtime';
+import { Routes, Route } from 'react-router-dom'
+import Layout from './layout/Layout';
+import GaragePage from './pages/garage/GaragePage';
+import WinnersPage from './pages/winners/WinnersPage';
+// import AppRouter from './routes/AppRouter';
 
-function App(): JSX.Element {
-  return <AppRouter />;
+// function App(): JSX.Element {
+//   return <AppRouter />;
+// }
+
+export function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<GaragePage />} />
+        <Route path="winners" element={<WinnersPage />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App;

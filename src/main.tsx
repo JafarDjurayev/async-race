c8b6ed6c2ea0585c32d75f7275de882a8de76aaf
JafarDjurayev <djorayevofficial@gmail.com>
2025-07-01 21:57:20 +0500

    fix: removed basename from router

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter basename="/async-race">
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>

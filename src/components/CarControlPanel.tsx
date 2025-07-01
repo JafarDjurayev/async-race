b088ff37663abbd93859ae92866d8370529f98d5
JafarDjurayev <djorayevofficial@gmail.com>
2025-07-01 15:03:36 +0500

    refactor: Fixed syntax issues, reduced amount of code lines and imported custom hooks for car control handling.

--- src/components/CarControlPanel.tsx
+++ src/components/CarControlPanel.tsx
@@ -1,44 +1,50 @@
-import React, { type JSX } from "react";
+import React from 'react';
 import '../styles/CarControlPanel.css';
-import { useCarStore, useCarUpdateStore } from "../app/zustand/useGarageStore";
-import { type CarControlPanelProps } from "../types/models";
-import { handleRemoveCar } from "../utils/GarageUtils";
+import { useCarControls } from '../utils/useCarControls';
+import type { CarControlPanelProps } from '../types/models';
 
+export default function CarControlPanel({ carId }: CarControlPanelProps) {
+  const { handleStartDrive, handleStop, handleSelect, handleRemove } =
+    useCarControls(carId);
 
-export default function CarControlPanel({ carId }: CarControlPanelProps): JSX.Element {
-
-    const cars = useCarStore((state) => state.cars);
-    const setSelectedCar = useCarUpdateStore((state) => state.setSelectedCar);
-  const handleSelectClick = () => {
-    const selected = cars.find((car) => car.id === carId);
-    if (selected) {
-      setSelectedCar(selected);
-      console.log('Selected car:', selected);
-    }else {
-    console.warn('No car found with ID:', carId); // ⚠️ Debug: invalid ID?
-  }
-  };
-    const handleClick = () => {
-        void handleRemoveCar(carId);
-    };
-    return (<>
-        <div className="car-control-panel">
-            <div>
-                <button type="button" className="car-select-button" onClick={handleSelectClick}>
-                    SELECT
-                </button>
-                <br />
-                <br />
-                <button type="button" className="car-remove-button" onClick={handleClick}>
-                    REMOVE
-                </button>
-            </div>
-            <div>
-                <button type="button" className="car-control-button">S</button>
-                <br />
-                <br />
-                <button type="button" className="car-control-button">R</button>
-            </div>
-        </div>
-    </>)
+  return (
+    <div className="car-control-panel">
+      <div>
+        <button
+          type="button"
+          className="car-select-button"
+          onClick={handleSelect}
+        >
+          SELECT
+        </button>
+        <br />
+        <br />
+        <button
+          type="button"
+          className="car-remove-button"
+          onClick={handleRemove}
+        >
+          REMOVE
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          className="car-control-button"
+          onClick={handleStartDrive}
+        >
+          S
+        </button>
+        <br />
+        <br />
+        <button
+          type="button"
+          className="car-control-button"
+          onClick={handleStop}
+        >
+          R
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,36 +1,70 @@
-import React, {type JSX} from 'react';
+import React, { type JSX } from 'react';
 import '../styles/Controls.css';
 import { useGenerateCars } from './HandleGenerateCars';
-import { CreateCarStore} from '../app/zustand/useGarageStore';
+import { CreateCarStore } from '../app/zustand/useGarageStore';
 import { useCarUpdateHandler } from '../utils/GarageUtils';
 import { useSyncCarInputs } from './HandleCarChanges';
+import { useRaceHandlers } from '../utils/UseRaceHandlers';
 
 export default function Controls(): JSX.Element {
-  const { nameValue, colorValue, handleNameChange,handleColorChange } = useSyncCarInputs();
+  const { nameValue, colorValue, handleNameChange, handleColorChange } =
+    useSyncCarInputs();
   const { handleGenerateClick } = useGenerateCars();
-  const setCarName = CreateCarStore((state) => state.setCarNameInput);
-  const setCarColor = CreateCarStore((state) => state.setCarColorInput);
-  const createCar = CreateCarStore((state) => state.createCar);
+  const setCarName = CreateCarStore(state => state.setCarNameInput);
+  const setCarColor = CreateCarStore(state => state.setCarColorInput);
+  const createCar = CreateCarStore(state => state.createCar);
   const { handleUpdateClick } = useCarUpdateHandler();
+  const { handleRaceAll, handleResetAll } = useRaceHandlers();
+
   return (
-    <>
-      <div className="controls">
-        <div className="race-buttons">
-          <button type="button">RACE</button>
-          <button type="button">RESET</button>
-        </div>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input type="text" placeholder="Car name" className="input-name" onChange={(e) => setCarName(e.target.value)}/>
-          <input type="color"  onChange={(e) => setCarColor(e.target.value)}/>
-          <button type="button" className="button-create" onClick={createCar}>CREATE</button>
-        </form>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input type="text" placeholder="Car name" className="input-name" value={nameValue}  onChange={handleNameChange}/>
-          <input type="color" className="color-input" value={colorValue}  onChange={handleColorChange}/>
-          <button type="button" className="button-update" onClick={handleUpdateClick}>UPDATE</button>
-        </form>
-        <button type="button" onClick={handleGenerateClick}>GENERATE</button>
+    <div className="controls">
+      <div className="race-buttons">
+        <button type="button" onClick={handleRaceAll}>
+          RACE
+        </button>
+        <button type="button" onClick={handleResetAll}>
+          RESET
+        </button>
       </div>
-    </>
+
+      <form onSubmit={e => e.preventDefault()}>
+        <input
+          type="text"
+          placeholder="Car name"
+          className="input-name"
+          onChange={e => setCarName(e.target.value)}
+        />
+        <input type="color" onChange={e => setCarColor(e.target.value)} />
+        <button type="button" className="button-create" onClick={createCar}>
+          CREATE
+        </button>
+      </form>
+
+      <form onSubmit={e => e.preventDefault()}>
+        <input
+          type="text"
+          placeholder="Car name"
+          className="input-name"
+          value={nameValue}
+          onChange={handleNameChange}
+        />
+        <input
+          type="color"
+          className="color-input"
+          value={colorValue}
+          onChange={handleColorChange}
+        />
+        <button
+          type="button"
+          className="button-update"
+          onClick={handleUpdateClick}
+        >
+          UPDATE
+        </button>
+      </form>
+      <button type="button" onClick={handleGenerateClick}>
+        GENERATE
+      </button>
+    </div>
   );
 }
